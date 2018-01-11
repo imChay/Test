cf90e6333bf025657e2b47ed06a581049c9c427b
changyan <1510207073@qq.com>
2018-01-11 14:33:54 +0800

    Initial Commit

---
+++ Test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Test
+//
+//  Created by cy on 2018/1/11.
+//  Copyright © 2018年 cy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
