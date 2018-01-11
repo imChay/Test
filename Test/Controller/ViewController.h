0f2447881dac2ace089c2a81fdcd50b7f03a8328
changyan <1510207073@qq.com>
2018-01-11 22:23:21 +0800

    init

---
+++ Test/Controller/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Test
+//
+//  Created by cy on 2018/1/11.
+//  Copyright © 2018年 cy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Test/Controller/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Test
+//
+//  Created by cy on 2018/1/11.
+//  Copyright © 2018年 cy. All rights reserved.
+//
+
+#import "ViewController.h"
+//#import "CYPrint.h"
+#import "LibTest.h"
+
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    [CYPrint print];
+}
+
+
+@end
